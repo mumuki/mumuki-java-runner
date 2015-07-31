fabaeb05aaa470e2207621b9ba825d4cd2d45e39
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-31 12:49:09 -0300

    Fixing integration tests

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,31 +1,29 @@
+require 'active_support/all'
 require 'mumukit/bridge'
 
 describe 'Server' do
-  let(:bridge) { Mumukit::Bridge::Bridge.new('http://localhost:4567') }
+  let(:bridge) { Mumukit::Bridge::Bridge.new('http://localhost:4568') }
 
   before(:all) do
-    @pid = Process.spawn 'rackup -p 4567', err: '/dev/null'
-    sleep 3
+    @pid = Process.spawn 'rackup -p 4568', err: '/dev/null'
+    sleep 8
   end
   after(:all) { Process.kill 'TERM', @pid }
 
   it 'answers a valid hash when submission passes' do
     response = bridge.run_tests!(test: %q{
+@Test
 public void testFoo() {
   Assert.assertEquals(Foo.bar(), 2);
 }}, extra: '', content: %q{
 class Foo {
   public static int bar() {
-    return 1;
+    return 2;
   }
 }}, expectations: [])
 
-    expect(response).to eq(status: :passed,
-                           result: "```\n.\n\n```",
-                           expectation_results: [],
-                           test_results: [],
-                           feedback: '',
-                           response_type: :unstructured)
+    expect(response[:result]).to include('OK')
+    expect(response[:status]).to eq(:passed)
   end
 
 
@@ -36,16 +34,12 @@ public void testFoo() {
 }}, extra: '', content: %q{
 class Foo {
   public static int bar() {
-    return 2;
+    return 1;
   }
 }}, expectations: [])
 
-    expect(response).to eq(status: :failed,
-                           result: "```\n.\n\n```",
-                           expectation_results: [],
-                           test_results: [],
-                           feedback: '',
-                           response_type: :unstructured)
+    expect(response[:status]).to eq(:failed)
+    expect(response[:result]).to include('There was 1 failure')
   end
 
 
