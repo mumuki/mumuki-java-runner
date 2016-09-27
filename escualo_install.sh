9e99f6ce60836302bc0229b059d10f4ba9eb314c
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-09-27 19:28:24 -0300

    Removing escualo config

--- escualo_install.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-REV=$1
-
-echo "[Escualo::JunitServer] Fetching GIT revision"
-echo -n $REV > version
-
-echo "[Escualo::JunitServer] Pulling docker image"
-docker pull mumuki/mumuki-junit-worker
