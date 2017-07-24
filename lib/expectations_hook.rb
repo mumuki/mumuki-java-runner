8644dab52df8fbbe2818e91d29fb78ca76120ed3
Franco Bulgarelli <franco@mumuki.org>
2017-07-24 01:39:43 -0300

    Adding mulang support

---
+++ lib/expectations_hook.rb
@@ -0,0 +1,11 @@
+class JavaExpectationsHook < Mumukit::Templates::MulangExpectationsHook
+  include_smells true
+
+  def language
+    'Java'
+  end
+
+  def default_smell_exceptions
+    LOGIC_SMELLS + FUNCTIONAL_SMELLS
+  end
+end
