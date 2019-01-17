a6189b40efeeedc30ba128b5249877e554128481
Rodrigo Alfonso <rodri042@gmail.com>
2019-01-17 18:48:22 -0300

    Adding expectations support

--- lib/expectations_hook.rb
+++ lib/expectations_hook.rb
@@ -1,11 +1,17 @@
-# class JavaExpectationsHook < Mumukit::Templates::MulangExpectationsHook
-#   include_smells true
-#
-#   def language
-#     'Java'
-#   end
-#
-#   def default_smell_exceptions
-#     LOGIC_SMELLS + FUNCTIONAL_SMELLS
-#   end
-# end
+class JavaExpectationsHook < Mumukit::Templates::MulangExpectationsHook
+  include_smells true
+
+  def language
+    'Java'
+  end
+
+  def compile_content(content)
+    return content unless content.is_a?(Hash)
+
+    content.values.join("\n").gsub(/import .+/, '')
+  end
+
+  def default_smell_exceptions
+    LOGIC_SMELLS + FUNCTIONAL_SMELLS
+  end
+end
