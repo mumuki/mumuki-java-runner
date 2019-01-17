50e5e8762a26203d1d327813b6cad559b2ef277d
Rodrigo Alfonso <rodri042@gmail.com>
2019-01-17 20:42:01 -0300

    Adding a spec with failing tests and expectations

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -34,9 +34,28 @@ class Foo {
                            result: '')
   end
 
-
-  it 'supports multiple files' do
+  it 'answers a valid hash when submission fails' do
     response = bridge.run_tests!(test: %q{
+@Test
+public void testFoo() {
+  Assert.assertEquals(Foo.bar(), 2);
+}}, extra: '', content: %q{
+class Foo {
+  public static int bar() {
+    return 2;
+  }
+}}, expectations: [])
+
+    expect(response).to eq(response_type: :structured,
+                           test_results: [{title: 'testFoo', status: :passed, result: nil}],
+                           status: :passed,
+                           feedback: '',
+                           expectation_results: [],
+                           result: '')
+  end
+
+  context 'multiple files support' do
+    let(:test) {%q{
 @Test
 public void email_admite_elaborar() {
   Email email = new Email(
@@ -44,7 +63,9 @@ public void email_admite_elaborar() {
     "Hola juan!!"
   );
   Assert.assertEquals("Para: Juan Perez <juanperez@gmail.com>\\nHola juan!!", email.elaborar());
-}}, extra: '', content: %q{
+}}}
+
+    let(:content) {%q{
 /*<Email.java#*/
 public class Email {
   private Destinatario destinatario;
@@ -76,39 +97,60 @@ public class Destinatario {
     }
 }
 /*#Destinatario.java>*/
-}, expectations: [{'binding' => 'Email',
-                    'inspection' => 'DeclaresMethod:elaborar'},
-                   {'binding' => 'Destinatario',
-                    'inspection' => 'DeclaresMethod:elaborarEncabezado'}])
+}}
 
-    expect(response).to eq(response_type: :structured,
-                           test_results: [{ title: 'email_admite_elaborar', status: :passed, result: nil }],
-                           status: :passed,
-                           feedback: '',
-                           expectation_results: [
-                               {:binding=>"Email", :inspection=>"DeclaresMethod:elaborar", :result=>:passed},
-                               {:binding=>"Destinatario", :inspection=>"DeclaresMethod:elaborarEncabezado", :result=>:passed}
-                           ],
-                           result: '')
-  end
+    let(:expectations) {
+      [{'binding' => 'Email',
+        'inspection' => 'DeclaresMethod:elaborar'},
+       {'binding' => 'Destinatario',
+        'inspection' => 'DeclaresMethod:elaborarEncabezado'}]
+    }
 
-  it 'answers a valid hash when submission fails' do
-    response = bridge.run_tests!(test: %q{
+    it 'with passing test and expectations' do
+      response = bridge.run_tests!(test: test, extra: '', content: content, expectations: expectations)
+
+      expect(response).to eq(response_type: :structured,
+                             test_results: [{ title: 'email_admite_elaborar', status: :passed, result: nil }],
+                             status: :passed,
+                             feedback: '',
+                             expectation_results: [
+                                 {:binding=>"Email", :inspection=>"DeclaresMethod:elaborar", :result=>:passed},
+                                 {:binding=>"Destinatario", :inspection=>"DeclaresMethod:elaborarEncabezado", :result=>:passed}
+                             ],
+                             result: '')
+    end
+
+    context 'with failing test and expectations' do
+      let(:test) {%q{
 @Test
-public void testFoo() {
-  Assert.assertEquals(Foo.bar(), 2);
-}}, extra: '', content: %q{
-class Foo {
-  public static int bar() {
-    return 2;
-  }
-}}, expectations: [])
+public void email_admite_elaborar() {
+  Email email = new Email(
+    new Destinatario("Juan Perez", "juanperez@gmail.com"),
+    "Hola juan!!"
+  );
+  Assert.assertEquals("Para: Juano Gomez <juanogomez@gmail.com>\\nHola juano!!", email.elaborar());
+}}}
 
-    expect(response).to eq(response_type: :structured,
-                           test_results: [{title: 'testFoo', status: :passed, result: nil}],
-                           status: :passed,
-                           feedback: '',
-                           expectation_results: [],
-                           result: '')
+      let(:expectations) {
+        [{'binding' => 'Email',
+          'inspection' => 'DeclaresMethod:elaboraroro'},
+         {'binding' => 'Destinatario',
+          'inspection' => 'DeclaresMethod:elaborarEncabezado'}]
+      }
+
+      it {
+        response = bridge.run_tests!(test: test, extra: '', content: content, expectations: expectations)
+
+        expect(response).to eq(response_type: :structured,
+                               test_results: [{ title: 'email_admite_elaborar', status: :failed, result: "\n```\nexpected:<Para: Juan[o Gomez <juanogomez@gmail.com>\nHola juano]!!> but was:<Para: Juan[ Perez <juanperez@gmail.com>\nHola juan]!!>\n```\n\n"}],
+                               status: :failed,
+                               feedback: '',
+                               expectation_results: [
+                                   {:binding=>"Email", :inspection=>"DeclaresMethod:elaboraroro", :result=>:failed},
+                                   {:binding=>"Destinatario", :inspection=>"DeclaresMethod:elaborarEncabezado", :result=>:passed}
+                               ],
+                               result: '')
+      }
+    end
   end
 end
